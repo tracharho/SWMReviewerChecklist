164fbf73b7b598e91f4959ba81df51ec3d51a39d
Travis Rhodes <trchrhapple@gmail.com>
2021-04-25 21:21:16 -0400

    Started branch to convert into desktop app with flaskwebgui module

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
-
+from flaskwebgui import FlaskUI
 
 app = Flask(__name__, static_folder='static')
 csrf = CSRFProtect(app)
@@ -12,6 +12,7 @@ csrf.init_app(app)
 app.config['ENV'] = 'development'
 app.config['DEBUG'] = True
 app.config['TESTING'] = True
+ui = FlaskUI(app, width=1200, height=1200)
 
 
 app.config.from_object(Config)
@@ -23,3 +24,4 @@ login.login_view = 'login' #requires user to be logged in to view certain pages
 
 
 from app import routes, models, makeletter
+ui.run()
